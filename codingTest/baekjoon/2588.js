8e3819f7c99fb530e0dae4cdde35abef6f427c38
Replit user <>
2023-07-12 07:48:17 +0000

    codingTest update

--- codingTest/baekjoon/2588.js
+++ codingTest/baekjoon/2588.js
@@ -9,14 +9,14 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 // console.log(a * parseInt(b / 100));
 // console.log(a * b);
 
-let a = input[0];
+let a = Number(input[0]);
 let b = input[1];
 
 let x_1 = b[2];
 let x_2 = b[1];
 let x_3 = b[0];
 
-console.log(a * x_1);
-console.log(a * x_2);
-console.log(a * x_3);
-console.log(a * b);
+console.log(a * Number(x_1));
+console.log(a * Number(x_2));
+console.log(a * Number(x_3));
+console.log(a * Number(b));
